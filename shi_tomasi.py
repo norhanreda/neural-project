da8c07fb45795d44ffe051c4fb5f46982279b47a
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-05-12 17:19:40 +0300

    adding try file to test our hands data

--- shi_tomasi.py
+++ shi_tomasi.py
@@ -7,10 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
 import joblib
 import cv2
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import VotingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+
 # Define the directory where the hand gesture images are stored
 dataset_dir = "dataset\dataset\Woman"
 # dataset_dir = "dataset_sample\Women"
@@ -18,15 +15,13 @@ dataset_dir = "dataset\dataset\Woman"
 labels = []
 features=[]
 
-# Define the HOG parameters
-win_size = (64, 64)
-block_size = (16, 16)
-block_stride = (8, 8)
-cell_size = (8, 8)
-nbins = 9
 
 lower_skin = np.array([0, 135, 85])
 upper_skin = np.array([255, 180, 135])
+
+max_corners = 500
+quality_level = 0.01
+min_distance = 0.1
 
 for sub_dir in os.listdir(dataset_dir):
     sub_dir_path = os.path.join(dataset_dir, sub_dir)
@@ -40,11 +35,12 @@ for sub_dir in os.listdir(dataset_dir):
         image_path = os.path.join(sub_dir_path, file_name)
 
         image = cv2.imread(image_path)
-        image= cv2.resize(image,(128,128))
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 
         ycrcb = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
         mask = cv2.inRange(ycrcb, lower_skin, upper_skin)
-
+        
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contour = max(contours, key = len)
 
@@ -56,11 +52,19 @@ for sub_dir in os.listdir(dataset_dir):
 
         new_img= cv2.resize(new_img,(128,128))
 
+
+        corners = cv2.goodFeaturesToTrack(mask, max_corners, quality_level, min_distance)
+        win_size = (64, 64)
+        block_size = (16, 16)
+        block_stride = (8, 8)
+        cell_size = (8, 8)
+        nbins = 9
         # Initialize HOG descriptor
         hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
-
         # Compute HOG features
         hog_features = hog.compute(new_img)
+        # print(len(corners.flatten()))
+        np.append(hog_features,corners.flatten())
         features.append(hog_features)
         print(sub_dir)
         labels.append(sub_dir)
@@ -72,7 +76,7 @@ print(labels.shape)
 
 # Split the dataset into training and testing sets
 train_features, test_features, train_labels, test_labels = train_test_split(
-    features, labels, test_size=0.25, random_state=22)
+    features, labels, test_size=0.25, random_state=42)
 
 print('Shape of train_images:', train_features.shape)
 print('Shape of train_labels:', train_labels.shape)
@@ -81,40 +85,15 @@ print('Shape of test_labels:', test_labels.shape)
 
 
 # Train a Support Vector Machine (SVM) classifier
-clf1 = svm.SVC(probability=True)
-clf2 = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=1000)
-clf3= RandomForestClassifier(n_estimators=200)
-# Create the soft voting classifier by combining the three base classifiers
-voting_clf = VotingClassifier(estimators=[('svc', clf1), ('mlp', clf2),('rdf', clf3)], voting='hard')
+svm_classifier = svm.SVC(kernel="linear")
+svm_classifier.fit(train_features, train_labels)
 
-# Train the voting classifier on the training data
-# voting_clf.fit(train_features, train_labels)
-
-# Evaluate the performance of the voting classifier on the testing data
-# accuracy = voting_clf.score(test_features, test_labels)
-# print("Accuracy:", accuracy*100)
-
-# Perform cross-validation
-scores = cross_val_score(voting_clf, features, labels, cv=8)
-
-# Print the accuracy scores for each fold
-print("Accuracy scores:", scores*100)
-
-# Calculate the mean and standard deviation of the scores
-mean_score = np.mean(scores)
-std_score = np.std(scores)
-
-# Print the mean and standard deviation of the scores
-print(f"Mean score: {mean_score*100}")
-print(f"Standard deviation: {std_score}")
-# svm_classifier.fit(train_features, train_labels)
-
-# Save the trained classifier to a file
+# # Save the trained classifier to a file
 # joblib.dump(svm_classifier, 'svm_classifier.joblib')
 
-# # Predict the labels of the test set using the trained SVM classifier
-# predicted_labels = svm_classifier.predict(test_features)
+# Predict the labels of the test set using the trained SVM classifier
+predicted_labels = svm_classifier.predict(test_features)
 
-# # Compute the accuracy of the SVM classifier
-# accuracy = accuracy_score(test_labels, predicted_labels)
-# print("Accuracy: {:.4f}%".format(accuracy * 100))
+# Compute the accuracy of the SVM classifier
+accuracy = accuracy_score(test_labels, predicted_labels)
+print("Accuracy: {:.4f}%".format(accuracy * 100))
